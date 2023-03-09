b06013cf9059b6f0e66a62f66c54d6ccdc09f3c4
CongQuoc <quoctncpd08011@fpt.edu.vn>
2023-03-09 12:19:40 +0700

    noplab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,10 +1,8 @@
 //nhap a tu ban phim 
 let a = prompt ("Nhap so nguyen a : ");
-let a1 = parseInt(a);
 //nhap b tu ban phim
-let b1 = prompt ("Nhap so nguyen  b : ");
+let b = prompt ("Nhap so nguyen  b : ");
 //nhap b tu ban phim
-let b= parseInt(b);
 let c = parseInt(a)+parseInt(b);
 document.write(c);
 console.log(C);
